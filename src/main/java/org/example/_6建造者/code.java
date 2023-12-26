86412a7664e9205f10e803f0c0384357e71130e7
刘梦飞 <liumengfei1@sunline.cn>
2023-12-26 17:47:04 +0800

    add：添加建造者设计模式代码；

--- src/main/java/org/example/_6建造者/code.java
+++ src/main/java/org/example/_6建造者/code.java
@@ -4,4 +4,111 @@ package org.example._6建造者;
  * 建造者模式
  */
 public class code {
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        worker.buildA("地基").buildB("墙体").buildC("窗户").buildD("屋顶");
+        Product product = worker.getProduct();
+        System.out.println(product.toString());
+    }
 }
+
+class Product {
+    private String buildA;
+    private String buildB;
+    private String buildC;
+    private String buildD;
+
+    public String getBuildA() {
+        return buildA;
+    }
+
+    public void setBuildA(String buildA) {
+        this.buildA = buildA;
+    }
+
+    public String getBuildB() {
+        return buildB;
+    }
+
+    public void setBuildB(String buildB) {
+        this.buildB = buildB;
+    }
+
+    public String getBuildC() {
+        return buildC;
+    }
+
+    public void setBuildC(String buildC) {
+        this.buildC = buildC;
+    }
+
+    public String getBuildD() {
+        return buildD;
+    }
+
+    public void setBuildD(String buildD) {
+        this.buildD = buildD;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "buildA='" + buildA + '\'' +
+                ", buildB='" + buildB + '\'' +
+                ", buildC='" + buildC + '\'' +
+                ", buildD='" + buildD + '\'' +
+                '}';
+    }
+}
+
+interface Builder {
+    Worker buildA(String msg);
+
+    Worker buildB(String msg);
+
+    Worker buildC(String msg);
+
+    Worker buildD(String msg);
+
+    Product getProduct();
+}
+
+class Worker implements Builder {
+    private Product product;
+
+    public Worker() {
+        product = new Product();
+    }
+
+    @Override
+    public Worker buildA(String msg) {
+        product.setBuildA(msg);
+        return this;
+    }
+
+    @Override
+    public Worker buildB(String msg) {
+        product.setBuildB(msg);
+        return this;
+    }
+
+    @Override
+    public Worker buildC(String msg) {
+        product.setBuildC(msg);
+        return this;
+    }
+
+    @Override
+    public Worker buildD(String msg) {
+        product.setBuildD(msg);
+        return this;
+    }
+
+    @Override
+    public Product getProduct() {
+        return product;
+    }
+}
+
+
+
