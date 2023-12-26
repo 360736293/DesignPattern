0f199e78322c647193ba9dff3773259b656ebe95
刘梦飞 <liumengfei1@sunline.cn>
2023-12-26 17:10:30 +0800

    add：添加工厂设计模式代码；

--- src/main/java/org/example/_2工厂/code.java
+++ src/main/java/org/example/_2工厂/code.java
@@ -4,4 +4,41 @@ package org.example._2工厂;
  * 工厂模式
  */
 public class code {
+    public static void main(String[] args) {
+        Car car = CarFactory.getCar("五菱");
+        Car car1 = CarFactory.getCar("特斯拉");
+        car.name();
+        car1.name();
+    }
+}
+
+class CarFactory {
+    public static Car getCar(String carName) {
+        switch (carName) {
+            case "五菱":
+                return new WuLing();
+            case "特斯拉":
+                return new Tesla();
+            default:
+                return null;
+        }
+    }
+}
+
+abstract class Car {
+    abstract void name();
+}
+
+class WuLing extends Car {
+    @Override
+    public void name() {
+        System.out.println("五菱宏光");
+    }
+}
+
+class Tesla extends Car {
+    @Override
+    public void name() {
+        System.out.println("特斯拉");
+    }
 }
