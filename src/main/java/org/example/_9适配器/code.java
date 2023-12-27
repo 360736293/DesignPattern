b71b25e8c1724130a539f7a1b7b8551072288db2
360736293 <360736293@qq.com>
2023-12-27 09:20:12 +0800

    add：添加适配器设计模式代码；

--- src/main/java/org/example/_9适配器/code.java
+++ src/main/java/org/example/_9适配器/code.java
@@ -4,4 +4,33 @@ package org.example._9适配器;
  * 适配器模式
  */
 public class code {
+    public static void main(String[] args) {
+        TargetFather target = new Adapter();
+        target.firstAct();
+    }
+}
+
+interface TargetFather {
+    void firstAct();
+}
+
+interface AdapteeFather {
+    void secondAct();
+}
+
+class Adaptee implements AdapteeFather {
+    @Override
+    public void secondAct() {
+        System.out.println("secondAct");
+    }
+}
+
+class Adapter implements TargetFather {
+    private Adaptee adaptee = new Adaptee();
+
+    @Override
+    public void firstAct() {
+        System.out.println("firstAct");
+        adaptee.secondAct();
+    }
 }
