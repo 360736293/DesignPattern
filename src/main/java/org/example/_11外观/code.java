485b260837a5063b10229623e9aad603fbf3960c
360736293 <360736293@qq.com>
2024-07-07 14:27:04 +0800

    add：添加外观(门面)设计模式；

---
+++ src/main/java/org/example/_11外观/code.java
@@ -0,0 +1,62 @@
+package org.example._11外观;
+
+/**
+ * 外观模式（又叫门面模式）
+ */
+public class code {
+    public static void main(String[] args) {
+        Facade fd = new Facade();
+
+        fd.doSomethingA();
+        fd.doSomethingB();
+        fd.doSomrthingC();
+    }
+}
+
+class ClassA {
+
+    public void doSomethingA() {
+        System.out.println("do something a");
+    }
+}
+
+class ClassB {
+
+    public void doSomethingB() {
+        System.out.println("do something b");
+    }
+}
+
+class ClassC {
+
+    public void doSomethingC() {
+        System.out.println("do something c");
+    }
+}
+
+class Facade {
+
+    //被委托对象
+    private ClassA ca;
+    private ClassB cb;
+    private ClassC cc;
+
+    public Facade() {
+        this.ca = new ClassA();
+        this.cb = new ClassB();
+        this.cc = new ClassC();
+    }
+
+    //提供给外部的访问方法
+    public void doSomethingA() {
+        this.ca.doSomethingA();
+    }
+
+    public void doSomethingB() {
+        this.cb.doSomethingB();
+    }
+
+    public void doSomrthingC() {
+        this.cc.doSomethingC();
+    }
+}
