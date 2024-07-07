354646ff7fedffc1f7bdc78f34436dad48a6fe11
刘梦飞 <liumengfei1@sunline.cn>
2024-07-07 14:08:38 +0800

    update：优化责任链模式的设计；

--- src/main/java/org/example/_5责任链/code.java
+++ src/main/java/org/example/_5责任链/code.java
@@ -1,72 +1,74 @@
 package org.example._5责任链;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 责任链模式
  */
 public class code {
     public static void main(String[] args) {
-        Leader leader = new Director("张三", 5);
-        Leader leader1 = new Manager("李四", 10);
-        Leader leader2 = new GeneralManager("王五", 15);
-        leader.setNextLeader(leader1);
-        leader1.setNextLeader(leader2);
-
+        LeaderChain leaderChain = new LeaderChain();
+        Leader leaderNode1 = new Director("张三", 5);
+        Leader leaderNode2 = new Manager("李四", 10);
+        Leader leaderNode3 = new GeneralManager("王五", 15);
+        leaderChain.addChain(leaderNode1).addChain(leaderNode2).addChain(leaderNode3);
         LeaveRequest leaveRequest = new LeaveRequest("小明", 6, "回家探亲");
-        leader.handleRequest(leaveRequest);
+        leaderChain.handleChain(leaveRequest);
+    }
+}
+
+class LeaderChain {
+    private List<Leader> chain = new ArrayList<>();
+
+    public LeaderChain addChain(Leader leader) {
+        this.chain.add(leader);
+        return this;
+    }
+
+    public void handleChain(LeaveRequest leaveRequest) {
+        for (Leader leader : chain) {
+            if (leaveRequest.getLeaveDays() <= leader.getDayNum()) {
+                System.out.println("领导：" + leader.getName() + "，批假最大天数：" + leader.getDayNum() + "，审批通过");
+                return;
+            } else {
+                System.out.println("领导：" + leader.getName() + "，批假最大天数：" + leader.getDayNum() + "，无审批权限，交由下一个领导审批");
+            }
+        }
     }
 }
 
 class Leader {
     private String name;
     private Integer dayNum;
-    private Leader nextLeader;
-
-    public Leader() {
-    }
 
     public Leader(String name, Integer dayNum) {
         this.name = name;
         this.dayNum = dayNum;
     }
 
-    public void setNextLeader(Leader nextLeader) {
-        this.nextLeader = nextLeader;
+    public String getName() {
+        return this.name;
     }
 
-    public void handleRequest(LeaveRequest leaveRequest) {
-        if (leaveRequest.getLeaveDays() <= this.dayNum) {
-            System.out.println("领导：" + this.name + "审批通过");
-        } else {
-            if (this.nextLeader != null) {
-                System.out.println("领导：" + this.name + " 无审批权限，交由 " + this.nextLeader.name + " 审批");
-                this.nextLeader.handleRequest(leaveRequest);
-            }
-        }
+    public Integer getDayNum() {
+        return this.dayNum;
     }
 }
 
 class Director extends Leader {
-    public Director() {
-    }
-
     public Director(String name, Integer dayNum) {
         super(name, dayNum);
     }
 }
 
 class Manager extends Leader {
-    public Manager() {
-    }
-
     public Manager(String name, Integer dayNum) {
         super(name, dayNum);
     }
 }
 
 class GeneralManager extends Leader {
-    public GeneralManager() {
-    }
-
     public GeneralManager(String name, Integer dayNum) {
         super(name, dayNum);
     }
@@ -77,14 +79,11 @@ class LeaveRequest {
     private int leaveDays;
     private String reason;
 
-    public LeaveRequest() {
-    }
-
     public LeaveRequest(String empName, int leaveDays, String reason) {
         this.empName = empName;
         this.leaveDays = leaveDays;
         this.reason = reason;
-        System.out.println("员工：" + this.empName + "请假 天数：" + this.leaveDays + " 理由：" + this.reason);
+        System.out.println("员工：" + this.empName + "，请假天数：" + this.leaveDays + "，理由：" + this.reason);
     }
 
     public String getEmpName() {
