116bd4ff6d422d4ac57b1d234c23a67ac6c9ba3f
360736293 <360736293@qq.com>
2024-10-27 06:05:49 +0800

    update：修改工厂模式的实现逻辑；

--- src/main/java/org/example/_2工厂/Code.java
+++ src/main/java/org/example/_2工厂/Code.java
@@ -5,25 +5,28 @@ package org.example._2工厂;
  */
 public class Code {
     public static void main(String[] args) {
-        Car car1 = CarFactory.getCar("五菱");
-        car1.name();
-        Car car2 = CarFactory.getCar("特斯拉");
-        car2.name();
-        Car car3 = CarFactory.getCar("骷髅马");
-        car3.name();
+        Factory wuLingCarFactory = new WuLingCarFactory();
+        wuLingCarFactory.getCar().name();
+        Factory teslaCarFactory = new TeslaCarFactory();
+        teslaCarFactory.getCar().name();
     }
 }
 
-class CarFactory {
-    public static Car getCar(String carName) {
-        switch (carName) {
-            case "五菱":
-                return new WuLing();
-            case "特斯拉":
-                return new Tesla();
-            default:
-                throw new RuntimeException("unknown car");
-        }
+interface Factory {
+    Car getCar();
+}
+
+class WuLingCarFactory implements Factory {
+    @Override
+    public Car getCar() {
+        return new WuLing();
+    }
+}
+
+class TeslaCarFactory implements Factory {
+    @Override
+    public Car getCar() {
+        return new Tesla();
     }
 }
 
